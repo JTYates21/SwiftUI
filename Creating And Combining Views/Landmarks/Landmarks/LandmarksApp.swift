33f806bcffbe3a874cef377826b1b21c5ea19703
JTYates21 <coolman232@gmail.com>
2022-04-05 09:22:37 -0600

    First Commit/Landmarks

---
+++ Creating And Combining Views/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jacek Yates on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
